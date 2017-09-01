d9cbff7f283f2d9a8c92396286f1bdbbd694d9b6
James Keats <james.keats@mymail.champlain.edu>
2017-08-31 22:03:47 -0400

    Refactor git process and text entry into different utility classes.

---
+++ Assets/KeatsLib/scripts/Editor/GitProcess.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+
+namespace UnityEditor
+{
+	public static class GitProcess
+	{
+		public static string Launch(string args, int bufferSize = 2048)
+		{
+			using (Process p = new Process())
+			{
+				p.StartInfo.CreateNoWindow = true;
+				p.StartInfo.UseShellExecute = false;
+				p.StartInfo.RedirectStandardOutput = true;
+				p.StartInfo.FileName = "git";
+				p.StartInfo.Arguments = args;
+				p.Start();
+
+				var buffer = new char[bufferSize];
+				p.StandardOutput.Read(buffer, 0, bufferSize);
+				p.WaitForExit();
+
+				return new string(buffer).TrimEnd('\n', '\0');
+			}
+		}
+		
+		public static string Launch(string path, string args, int bufferSize = 2048)
+		{
+			using (Process p = new Process())
+			{
+				p.StartInfo.CreateNoWindow = true;
+				p.StartInfo.UseShellExecute = false;
+				p.StartInfo.RedirectStandardOutput = true;
+				p.StartInfo.WorkingDirectory = path;
+				p.StartInfo.FileName = "git";
+				p.StartInfo.Arguments = args;
+				p.Start();
+
+				var buffer = new char[bufferSize];
+				p.StandardOutput.Read(buffer, 0, bufferSize);
+				p.WaitForExit();
+
+				return new string(buffer).TrimEnd('\n', '\0');
+			}
+		}
+	}
+}
