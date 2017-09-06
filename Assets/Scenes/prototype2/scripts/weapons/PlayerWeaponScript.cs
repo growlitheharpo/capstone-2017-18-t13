4063ded34e851d3bd9d98104d4370d40183685d5
James Keats <james.keats@mymail.champlain.edu>
2017-09-05 23:05:01 -0400

    Start implementing weapon data.

---
+++ Assets/Scenes/prototype2/scripts/weapons/PlayerWeaponScript.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Prototype2
+{
+	public class PlayerWeaponScript : MonoBehaviour
+	{
+		[Serializable]
+		public struct WeaponData
+		{
+			[SerializeField] private float mDefaultSpread;
+			[SerializeField] private float mDefaultDamage;
+			[SerializeField] private float mFireRate;
+			[SerializeField] [Range(0.0f, 1.0f)] private float mDefaultRecoil;
+
+			public float defaultSpread { get { return mDefaultSpread; } }
+			public float defaultDamage { get { return mDefaultDamage; } }
+			public float fireRate { get { return mFireRate; } }
+			public float defaultRecoil { get { return mDefaultRecoil; } }
+
+			public WeaponData(WeaponData other)
+			{
+				mDefaultSpread = other.mDefaultSpread;
+			}
+
+			// Clip size, reload speed
+		}
+
+		[SerializeField] private WeaponData mBaseData;
+		[SerializeField] private Transform mBarrelAttach;
+		[SerializeField] private Transform mScopeAttach;
+
+		
+	}
+}
