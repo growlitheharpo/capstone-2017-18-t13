eb4d4285db3bc3ecd8d2eb9103ec2c282979da54
James Keats <james.keats@mymail.champlain.edu>
2017-09-08 11:03:19 -0400

    Make the cursor-handling code more efficient.

--- Assets/Scenes/prototype2/scripts/state/GamestateManager$Prototype2State.cs
+++ Assets/Scenes/prototype2/scripts/state/GamestateManager$Prototype2State.cs
@@ -8,22 +8,36 @@ public partial class GamestateManager
 	/// <summary>
 	/// State used when the game is in the game scene state.
 	/// </summary>
+	/// <inheritdoc />
 	private class Prototype2State : BaseGameState
 	{
-		private IInput mInputRef;
-
 		/// <inheritdoc />
 		public override void OnEnter()
 		{
-			mInputRef = ServiceLocator.Get<IInput>();
-			mInputRef.EnableInputLevel(Input.InputLevel.Gameplay);
+			EventManager.OnInputLevelChanged += HandleInputChange;
+
+			bool state = ServiceLocator.Get<IInput>().IsInputEnabled(Input.InputLevel.Gameplay);
+			SetCursorState(state);
 		}
 
-		public override void Update()
+		private void HandleInputChange(Input.InputLevel input, bool state)
 		{
-			bool playing = mInputRef.IsInputEnabled(Input.InputLevel.Gameplay);
+			if (input != Input.InputLevel.Gameplay)
+				return;
+
+			SetCursorState(state);
+		}
+
+		private void SetCursorState(bool playing)
+		{
 			Cursor.lockState = playing ? CursorLockMode.Locked : CursorLockMode.None;
 			Cursor.visible = !playing;
+		}
+
+		public override void OnExit()
+		{
+			EventManager.OnInputLevelChanged -= HandleInputChange;
+			SetCursorState(false);
 		}
 	}
 }
