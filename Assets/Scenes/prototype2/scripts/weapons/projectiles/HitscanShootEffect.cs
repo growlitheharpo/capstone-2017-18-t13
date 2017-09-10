b37350f561229e67c1830851ea8a13c823cc61eb
James Keats <james.keats@mymail.champlain.edu>
2017-09-10 17:59:06 -0400

    Do bullet effect in world space instead of local.

--- Assets/Scenes/prototype2/scripts/weapons/projectiles/HitscanShootEffect.cs
+++ Assets/Scenes/prototype2/scripts/weapons/projectiles/HitscanShootEffect.cs
@@ -12,18 +12,19 @@ public class HitscanShootEffect : MonoBehaviour
 
 	public IEnumerator Flash(Vector3 end, float time = 0.15f, Space endSpace = Space.World)
 	{
-		Vector3 realEnd = endSpace == Space.World ? transform.InverseTransformPoint(end) : end;
+		Vector3 realEnd = endSpace == Space.World ? end : transform.TransformPoint(end);
+		Vector3 start = transform.position;
 
 		float currentTime = 0.0f;
 		float moveTime = time / 2.0f;
 
 		mRenderer.positionCount = 2;
-		mRenderer.SetPosition(0, Vector3.zero);
-		mRenderer.SetPosition(1, Vector3.zero);
+		mRenderer.SetPosition(0, start);
+		mRenderer.SetPosition(1, start);
 
 		while (currentTime < moveTime)
 		{
-			mRenderer.SetPosition(1, Vector3.Lerp(Vector3.zero, realEnd, currentTime / moveTime));
+			mRenderer.SetPosition(1, Vector3.Lerp(start, realEnd, currentTime / moveTime));
 			currentTime += Time.deltaTime;
 			yield return null;
 		}
@@ -31,7 +32,7 @@ public class HitscanShootEffect : MonoBehaviour
 		currentTime = 0.0f;
 		while (currentTime < moveTime)
 		{
-			mRenderer.SetPosition(0, Vector3.Lerp(Vector3.zero, realEnd, currentTime / moveTime));
+			mRenderer.SetPosition(0, Vector3.Lerp(start, realEnd, currentTime / moveTime));
 			currentTime += Time.deltaTime;
 			yield return null;
 		}
