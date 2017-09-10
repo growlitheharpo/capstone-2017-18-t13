562615cff186f257069176dd6dd0c1460b104505
James Keats <james.keats@mymail.champlain.edu>
2017-09-09 23:42:21 -0400

    Refactor reloading and add an animation.

---
+++ Assets/KeatsLib/scripts/utility/WaitForAnimation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class WaitForAnimation : CustomYieldInstruction
+{
+	private readonly Animator mAnim;
+	private readonly int mNameHash;
+	private readonly int mDepth;
+
+	public WaitForAnimation(Animator anim, int depth = 0)
+	{
+		mAnim = anim;
+		mDepth = depth;
+		mNameHash = mAnim.GetCurrentAnimatorStateInfo(depth).fullPathHash;
+
+	}
+
+	public override bool keepWaiting
+	{
+		get { return mAnim.GetCurrentAnimatorStateInfo(mDepth).fullPathHash == mNameHash; }
+	}
+}
