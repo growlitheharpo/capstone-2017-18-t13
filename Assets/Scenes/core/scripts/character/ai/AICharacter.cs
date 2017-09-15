fd6593b4a53333af846e3a558a074ee25b447704
James Keats <james.keats@mymail.champlain.edu>
2017-09-15 12:15:25 -0400

    Add base AI classes that are empty.

---
+++ Assets/Scenes/core/scripts/character/ai/AICharacter.cs
@@ -0,0 +1,70 @@
+﻿using FiringSquad.Data;
+using KeatsLib;
+using UnityEngine;
+
+namespace FiringSquad.Gameplay
+{
+	public class AICharacter : MonoBehaviour, IWeaponBearer, IDamageReceiver
+	{
+		[SerializeField] private GameObject mGunPrefab;
+		[SerializeField] private WeaponDefaultsData mGunDefaultParts;
+		[SerializeField] private float mDefaultHealth;
+
+		private BoundProperty<float> mCurrentHealth;
+		private AIDecisionMaker mDecisionMaker;
+		private AIWeaponScript mWeapon;
+		private Transform mFakeEye;
+
+		public Transform eye { get { return mFakeEye; } }
+
+		private void Awake()
+		{
+			mDecisionMaker = new AIDecisionMaker();
+			mFakeEye = transform.Find("FakeAIEye");
+
+			var offset = transform.Find("Gun1Offset");
+			var gun = UnityUtils.InstantiateIntoHolder(mGunPrefab, offset, true, true);
+			mWeapon = gun.GetComponent<AIWeaponScript>();
+		}
+
+		// Use this for initialization
+		private void Start()
+		{
+			mCurrentHealth = new BoundProperty<float>(mDefaultHealth, (name + "-health").GetHashCode());
+
+			foreach (GameObject part in mGunDefaultParts)
+				Instantiate(part).GetComponent<WeaponPickupScript>().ConfirmAttach(mWeapon);
+		}
+
+		private void OnDestroy()
+		{
+			mCurrentHealth.Cleanup();
+		}
+
+		// Update is called once per frame
+		private void Update()
+		{
+			mDecisionMaker.Tick();
+		}
+
+		public void ApplyRecoil(Vector3 direction, float amount)
+		{
+			// TODO: How will the AI respond to recoil?
+		}
+
+		public void ApplyDamage(float amount, Vector3 point, IDamageSource cause)
+		{
+			StopAllCoroutines();
+			mCurrentHealth.value = Mathf.Clamp(mCurrentHealth.value - amount, 0.0f, float.MaxValue);
+
+			if (mCurrentHealth.value <= 0.0f)
+				Die();
+		}
+
+		private void Die()
+		{
+			// TODO: Implement death
+			Logger.Info("\"I just died :(\" - " + name);
+		}
+	}
+}
---
+++ Assets/Scenes/core/scripts/character/ai/AIDecisionMaker.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using UnityEngine;
+
+namespace FiringSquad.Gameplay
+{
+	public class AIDecisionMaker
+	{
+		private Action mCurrentState;
+
+		public AIDecisionMaker()
+		{
+			mCurrentState = TickIdle;
+		}
+
+		public void Tick()
+		{
+			mCurrentState.Invoke();
+		}
+
+		#region State Functions
+
+		private void TickIdle()
+		{
+			// just hang out. play a bark. scratch your head.
+		}
+
+		private void TickPurposefullyMissPlayer()
+		{
+			// instruct our gun to shoot near the player, but not AT them.
+		}
+
+		private void TickShootAtPlayer()
+		{
+			// shoot at the player, with some level of inaccuracy.
+		}
+
+		private void TickChaseAfterPlayer()
+		{
+			// run towards the player with a set offset.
+		}
+
+		private void TickChaseAndShoot()
+		{
+			// do both of the previous
+			TickChaseAfterPlayer();
+			TickShootAtPlayer();
+		}
+
+		private void TickLostPlayer()
+		{
+			// we lost the player. check for a timeout, then return to idle or the appropriate state.
+		}
+
+		#endregion
+
+		#region Common Utility Funcions
+
+		private bool KnowsOfPlayer()
+		{
+			return CanSeePlayer() || AttackedByPlayer();
+		}
+
+		private bool CanSeePlayer()
+		{
+			return false;
+		}
+
+		private bool AttackedByPlayer()
+		{
+			return false;
+		}
+
+		private Transform GetPlayerTransform()
+		{
+			return null;
+		}
+
+		private Action DecideToChaseOrShoot()
+		{
+			// return TickShootAtPlayer, TickChaseAfterPlayer, or TickChaseAndShoot
+			return null;
+		}
+
+		#endregion
+	}
+}
