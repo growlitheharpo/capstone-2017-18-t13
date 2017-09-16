d0682386bcf3a4bfbf6972ea9fc60f2cd36091ac
James Keats <james.keats@mymail.champlain.edu>
2017-09-16 13:28:33 -0400

    Add an intro scene and demo weapon.

---
+++ Assets/Scenes/prototype1/scripts/weapon/DemoWeaponScript.cs
@@ -0,0 +1,21 @@
+﻿using FiringSquad.Data;
+using UnityEngine;
+
+namespace FiringSquad.Gameplay
+{
+	public class DemoWeaponScript : BaseWeaponScript
+	{
+		[SerializeField] private float mRotationRate;
+
+		public WeaponData currentStats { get { return mCurrentData; } }
+
+		protected override void PlayShotEffect() { }
+		protected override void PlayReloadEffect(float time) { }
+
+		protected override void Update()
+		{
+			base.Update();
+			transform.Rotate(Vector3.up, mRotationRate * Time.deltaTime);
+		}
+	}
+}
