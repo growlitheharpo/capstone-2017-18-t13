ef97decea442a8bea474589a830d97d5c6402bf3
James Keats <james.keats@mymail.champlain.edu>
2017-09-16 13:36:36 -0400

    Add the manager script.

---
+++ Assets/Scenes/prototype1/scripts/intro/IntroWeaponChoiceManager.cs
@@ -0,0 +1,26 @@
+﻿using FiringSquad.Data;
+using UnityEngine;
+
+namespace FiringSquad.Gameplay
+{
+	public class IntroWeaponChoiceManager : MonoBehaviour
+	{
+		[SerializeField] private WeaponDefaultsData mDefaultParts;
+
+		private WeaponDefaultsData mChosenParts;
+		private DemoWeaponScript mDemoWeapon;
+
+		private void Awake()
+		{
+			mChosenParts = new WeaponDefaultsData(mDefaultParts);
+		}
+
+		private void Start()
+		{
+			mDemoWeapon = FindObjectOfType<DemoWeaponScript>();
+			
+			foreach (GameObject part in mChosenParts)
+				Instantiate(part).GetComponent<WeaponPickupScript>().ConfirmAttach(mDemoWeapon);
+		}
+	}
+}
