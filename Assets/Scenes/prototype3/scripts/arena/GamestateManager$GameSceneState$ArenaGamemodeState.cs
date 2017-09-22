da4742291011cf549de7907104f64c76dfdab8de
James Keats <james.keats@mymail.champlain.edu>
2017-09-22 11:49:03 -0400

    Change arena mode to be another HSM.

--- Assets/Scenes/prototype3/scripts/arena/GamestateManager$GameSceneState$ArenaGamemodeState.cs
+++ Assets/Scenes/prototype3/scripts/arena/GamestateManager$GameSceneState$ArenaGamemodeState.cs
@@ -6,7 +6,6 @@ using KeatsLib.Collections;
 using KeatsLib.State;
 using KeatsLib.Unity;
 using UnityEngine;
-using Input = KeatsLib.Unity.Input;
 
 public partial class GamestateManager
 {
@@ -34,79 +33,21 @@ public partial class GamestateManager
 				mPlayer2Score = new BoundProperty<int>(0, GameplayUIManager.PLAYER2_SCORE);
 				mRemainingTime = new BoundProperty<float>(mSettings.roundTime, GameplayUIManager.ARENA_ROUND_TIME);
 
-				EventManager.OnPlayerDied += HandlePlayerDeath;
-				GeneratePlayerList();
-				GenerateSpawnList();
-				MovePlayersToSpawn();
-			}
-
-			private void GeneratePlayerList()
-			{
-				mPlayerList = FindObjectsOfType<PlayerScript>().OrderBy(x => x.name).ToArray();
-			}
-
-			private void GenerateSpawnList()
-			{
-				mSpawnPoints = mSettings.spawnPoints;
-			}
-
-			private void MovePlayersToSpawn()
-			{
-				var spawnsCopy = new List<Transform>(mSpawnPoints);
-				foreach (PlayerScript player in mPlayerList)
-				{
-					Transform t = spawnsCopy.ChooseRandom();
-					spawnsCopy.Remove(t);
-
-					player.transform.position = t.position;
-					player.transform.rotation = t.rotation;
-				}
+				TransitionStates(new StartGameState(this));
 			}
 
 			public new void Update()
 			{
 				base.Update();
-				mRemainingTime.value -= Time.deltaTime;
-
-				if (mRemainingTime.value <= 0.0f)
-					HandleMatchEnd();
-			}
-
-			private void HandleMatchEnd()
-			{
-				ServiceLocator.Get<IInput>()
-					.DisableInputLevel(Input.InputLevel.Gameplay);
-			}
-
-			private void HandlePlayerDeath(ICharacter obj)
-			{
-				if (ReferenceEquals(obj, mPlayerList[0]))
-					mPlayer2Score.value += 1;
-				else if (ReferenceEquals(obj, mPlayerList[1]))
-					mPlayer1Score.value += 1;
-				else
-					throw new ArgumentException("We got an invalid player from OnPlayerDied!");
-
-				PlayerScript player = (PlayerScript)obj;
-				Transform t = mSpawnPoints.ChooseRandom();
-
-				if (settings.deathParticles != null)
-				{
-					ParticleSystem ps = Instantiate(settings.deathParticles, player.transform.position, Quaternion.identity)
-						.GetComponent<ParticleSystem>();
-					ps.Play();
-					instance.StartCoroutine(Coroutines.WaitAndDestroyParticleSystem(ps));
-				}
-
-				player.transform.position = t.position;
-				player.transform.rotation = t.rotation;
-
-				player.ResetArenaPlayer();
 			}
 
 			public void OnExit()
 			{
-				EventManager.OnPlayerDied -= HandlePlayerDeath;
+				currentState.OnExit();
+
+				mPlayer1Score.Cleanup();
+				mPlayer2Score.Cleanup();
+				mRemainingTime.Cleanup();
 			}
 
 			public IState GetTransition()
@@ -114,15 +55,114 @@ public partial class GamestateManager
 				return this;
 			}
 
+			#region States
+
+			private class StartGameState : BaseState<ArenaGamemodeState>
+			{
+				public StartGameState(ArenaGamemodeState machine) : base(machine) { }
+
+				public override void OnEnter()
+				{
+					mMachine.mRemainingTime.value = mMachine.settings.roundTime;
+					mMachine.mPlayer1Score.value = 0;
+					mMachine.mPlayer2Score.value = 0;
+
+					GeneratePlayerList();
+					GenerateSpawnList();
+
+					MovePlayersToSpawn();
+				}
+
+				private void GeneratePlayerList()
+				{
+					mMachine.mPlayerList = FindObjectsOfType<PlayerScript>().OrderBy(x => x.name).ToArray();
+				}
+
+				private void GenerateSpawnList()
+				{
+					mMachine.mSpawnPoints = mMachine.mSettings.spawnPoints;
+				}
+
+				private void MovePlayersToSpawn()
+				{
+					var spawnsCopy = new List<Transform>(mMachine.mSpawnPoints);
+					foreach (PlayerScript player in mMachine.mPlayerList)
+					{
+						Transform t = spawnsCopy.ChooseRandom();
+						spawnsCopy.Remove(t);
+
+						player.transform.position = t.position;
+						player.transform.rotation = t.rotation;
+					}
+				}
+
+				public override IState GetTransition()
+				{
+					return new PlayingState(mMachine);
+				}
+			}
+
 			private class PlayingState : BaseState<ArenaGamemodeState>
 			{
 				public PlayingState(ArenaGamemodeState machine) : base(machine) { }
+
+				public override void OnEnter()
+				{
+					EventManager.OnPlayerDied += HandlePlayerDeath;
+				}
+
+				public override void Update()
+				{
+					mMachine.mRemainingTime.value -= Time.deltaTime;
+				}
+
+				private void HandlePlayerDeath(ICharacter obj)
+				{
+					if (ReferenceEquals(obj, mMachine.mPlayerList[0]))
+						mMachine.mPlayer2Score.value += 1;
+					else if (ReferenceEquals(obj, mMachine.mPlayerList[1]))
+						mMachine.mPlayer1Score.value += 1;
+					else
+						throw new ArgumentException("We got an invalid player from OnPlayerDied!");
+
+					PlayerScript player = (PlayerScript)obj;
+					Transform t = mMachine.mSpawnPoints.ChooseRandom();
+
+					if (mMachine.settings.deathParticles != null)
+					{
+						ParticleSystem ps = Instantiate(mMachine.settings.deathParticles, player.transform.position, Quaternion.identity)
+							.GetComponent<ParticleSystem>();
+						ps.Play();
+						instance.StartCoroutine(Coroutines.WaitAndDestroyParticleSystem(ps));
+					}
+
+					player.transform.position = t.position;
+					player.transform.rotation = t.rotation;
+
+					player.ResetArenaPlayer();
+				}
+
+				public override IState GetTransition()
+				{
+					return (mMachine.mRemainingTime.value <= 0.0f) ? (IState)new EndMatchState(mMachine) : this;
+				}
+
+				public override void OnExit()
+				{
+					EventManager.OnPlayerDied -= HandlePlayerDeath;
+				}
+			}
+
+			private class EndMatchState : BaseState<ArenaGamemodeState>
+			{
+				public EndMatchState(ArenaGamemodeState machine) : base(machine) { }
 
 				public override IState GetTransition()
 				{
 					return this;
 				}
 			}
+			#endregion
 		}
 	}
 }
