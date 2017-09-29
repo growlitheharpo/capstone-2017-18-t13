f5347a51a6b844bcc657b11b62c70d737e2adca1
James Keats <james.keats@mymail.champlain.edu>
2017-09-28 23:40:13 -0400

    Create a script for LAN discovery.

---
+++ Assets/Scenes/sandbox/OverrideNetworkDiscovery.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class OverrideNetworkDiscovery : NetworkDiscovery
+{
+	private void Start()
+	{
+		Initialize();
+	}
+
+	private void OnGUI()
+	{
+		Rect rect = new Rect(0.0f, Screen.height - 200.0f, 200.0f, 200.0f);
+		GUILayout.BeginArea(rect);
+
+		if (GUILayout.Button("Start As Server"))
+			StartAsServer();
+		if (GUILayout.Button("Start As Client"))
+			StartAsClient();
+		GUILayout.EndArea();
+	}
+
+	public override void OnReceivedBroadcast(string fromAddress, string data)
+	{
+		NetworkManager manager = FindObjectOfType<NetworkManager>();
+		manager.networkAddress = fromAddress;
+		manager.StartClient();
+
+		StopBroadcast();
+	}
+}
