ceb2208fe2723d0139ad5e5690b6a2f6063973df
James Keats <james.keats@mymail.champlain.edu>
2017-09-29 11:36:50 -0400

    Remove unused network discovery code.

--- Assets/Scenes/core/scripts/networking/OverrideNetworkDiscovery.cs
+++ Assets/Scenes/core/scripts/networking/OverrideNetworkDiscovery.cs
@@ -1,25 +1,7 @@
-﻿using UnityEngine;
-using UnityEngine.Networking;
+﻿using UnityEngine.Networking;
 
 public class OverrideNetworkDiscovery : NetworkDiscovery
 {
-	private void Start()
-	{
-		Initialize();
-	}
-
-	private void OnGUI()
-	{
-		Rect rect = new Rect(0.0f, Screen.height - 200.0f, 200.0f, 200.0f);
-		GUILayout.BeginArea(rect);
-
-		if (GUILayout.Button("Start As Server"))
-			StartAsServer();
-		if (GUILayout.Button("Start As Client"))
-			StartAsClient();
-		GUILayout.EndArea();
-	}
-
 	public override void OnReceivedBroadcast(string fromAddress, string data)
 	{
 		NetworkManager manager = FindObjectOfType<NetworkManager>();
