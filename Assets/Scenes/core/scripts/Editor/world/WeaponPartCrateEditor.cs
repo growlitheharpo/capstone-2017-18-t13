5796e175728ffedfaf93ad19bfd0c20df7937604
James Keats <james.keats@mymail.champlain.edu>
2017-10-20 16:28:06 -0400

    Add the crate script and prefabs.

---
+++ Assets/Scenes/core/scripts/Editor/world/WeaponPartCrateEditor.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+namespace UnityEditor
+{
+	[CustomEditor(typeof(FiringSquad.Gameplay.WeaponPartCrate))]
+	public class WeaponPartCrateEditor : Editor
+	{
+		private UnityEditorInternal.ReorderableList mList;
+		private SerializedProperty mPartsProperty;
+
+		private void OnEnable()
+		{
+			mPartsProperty = serializedObject.FindProperty("mParts");
+			mList =
+				new UnityEditorInternal.ReorderableList(serializedObject, mPartsProperty, true, true, true, true)
+				{
+					drawHeaderCallback = rect =>
+					{
+						EditorGUI.LabelField(rect, "Parts and Weights");
+					},
+					drawElementCallback = (rect, index, isActive, isFocused) =>
+					{
+						SerializedProperty element = mList.serializedProperty.GetArrayElementAtIndex(index);
+						EditorGUI.PropertyField(new Rect(rect.x, rect.y, 150.0f, EditorGUIUtility.singleLineHeight),
+							element.FindPropertyRelative("mPrefab"), GUIContent.none, true);
+
+						EditorGUI.PropertyField(new Rect(rect.x + 150.0f, rect.y, rect.width - 150.0f, EditorGUIUtility.singleLineHeight),
+							element.FindPropertyRelative("mWeight"), GUIContent.none, true);
+					}
+				};
+		}
+
+		public override void OnInspectorGUI()
+		{
+			serializedObject.Update();
+			DrawDefaultInspector();
+
+			mList.DoLayoutList();
+			BalanceWeights();
+
+			serializedObject.ApplyModifiedProperties();
+		}
+
+		private void BalanceWeights()
+		{
+			float sum = 0.0f;
+			for (int i = 0; i < mPartsProperty.arraySize; i++)
+				sum += mPartsProperty.GetArrayElementAtIndex(i).FindPropertyRelative("mWeight").floatValue;
+
+			if (sum <= 0.0f)
+				return;
+
+			for (int i = 0; i < mPartsProperty.arraySize; i++)
+				mPartsProperty.GetArrayElementAtIndex(i).FindPropertyRelative("mWeight").floatValue /= sum;
+		}
+	}
+}
---
+++ Assets/Scenes/core/scripts/world/WeaponPartCrate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+namespace FiringSquad.Gameplay
+{
+	public class WeaponPartCrate : NetworkBehaviour
+	{
+		[Serializable]
+		public struct PartWeightSet
+		{
+			public GameObject mPrefab;
+			[Range(0.0f, 1.0f)] public float mWeight;
+		}
+
+		[HideInInspector] [SerializeField] List<PartWeightSet> mParts;
+		[SerializeField] private GameObject mBreakVFX;
+		public float mRespawnTime;
+
+		// Use this for initialization
+		void Start() { }
+
+		// Update is called once per frame
+		void Update() { }
+	}
+}
