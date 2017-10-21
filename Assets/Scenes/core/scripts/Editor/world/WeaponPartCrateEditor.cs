6dc758a851fd4b0215a10b33de09159b38bbbdf3
James Keats <james.keats@mymail.champlain.edu>
2017-10-20 20:38:39 -0400

    Add snap to floor button.

--- Assets/Scenes/core/scripts/Editor/world/WeaponPartCrateEditor.cs
+++ Assets/Scenes/core/scripts/Editor/world/WeaponPartCrateEditor.cs
@@ -38,6 +38,9 @@ namespace UnityEditor
 			mList.DoLayoutList();
 			BalanceWeights();
 
+			if (GUILayout.Button("Snap To Floor"))
+				SnapToFloor((target) as Component);
+
 			serializedObject.ApplyModifiedProperties();
 		}
 
@@ -52,6 +55,21 @@ namespace UnityEditor
 
 			for (int i = 0; i < mPartsProperty.arraySize; i++)
 				mPartsProperty.GetArrayElementAtIndex(i).FindPropertyRelative("mWeight").floatValue /= sum;
+		}
+
+		private void SnapToFloor(Component component)
+		{
+			Transform t = component.transform;
+			Ray r = new Ray(t.position, Vector3.down);
+
+			RaycastHit hitInfo;
+			if (!Physics.Raycast(r, out hitInfo))
+				return;
+
+			Collider col = component.GetComponent<Collider>();
+			Vector3 point = hitInfo.point;
+			point.y += col.bounds.extents.y;
+			t.position = point;
 		}
 	}
 }
