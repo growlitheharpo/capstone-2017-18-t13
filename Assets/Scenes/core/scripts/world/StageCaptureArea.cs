3aaedf1f26860e95b6992b83799f6f16b6000c43
James Keats <james.keats@mymail.champlain.edu>
2017-10-29 12:34:35 -0400

    Setup a collision forwarder on the trigger.

---
+++ Assets/Scenes/core/scripts/world/StageCaptureArea.cs
@@ -0,0 +1,27 @@
+﻿using KeatsLib.Unity;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace FiringSquad.Gameplay
+{
+	public class StageCaptureArea : NetworkBehaviour
+	{
+		[SerializeField] private CollisionForwarder mTrigger;
+
+		private void Awake()
+		{
+			mTrigger.mTriggerEnterDelegate = OnTriggerEnter;
+			mTrigger.mTriggerExitDelegate = OnTriggerExit;
+		}
+
+		private void OnTriggerEnter(Collider other)
+		{
+			
+		}
+
+		private void OnTriggerExit(Collider other)
+		{
+			
+		}
+	}
+}
