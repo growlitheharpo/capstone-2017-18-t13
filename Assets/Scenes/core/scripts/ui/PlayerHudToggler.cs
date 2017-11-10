270f6f16e620cd64fd61bcd0592dc30a764a1bdb
James Keats <james.keats@mymail.champlain.edu>
2017-11-10 16:19:47 -0500

    Add a script to disable and enable the player's HUD.

---
+++ Assets/Scenes/core/scripts/ui/PlayerHudToggler.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace FiringSquad.Gameplay.UI
+{
+	/// <summary>
+	/// Disables the player's HUD when the game starts, then enables it when the player spawns.
+	/// </summary>
+	public class PlayerHudToggler : MonoBehaviour
+	{
+		/// <summary>
+		/// Unity's Awake function.
+		/// </summary>
+		private void Awake()
+		{
+			EventManager.Local.OnLocalPlayerSpawned += OnLocalPlayerSpawned;
+			gameObject.SetActive(false);
+		}
+
+		/// <summary>
+		/// Cleanup listeners and event handlers.
+		/// </summary>
+		private void OnDestroy()
+		{
+			EventManager.Local.OnLocalPlayerSpawned -= OnLocalPlayerSpawned;
+		}
+
+		/// <summary>
+		/// EVENT HANDLER: Local.OnLocalPlayerSpawned
+		/// </summary>
+		private void OnLocalPlayerSpawned(CltPlayer obj)
+		{
+			gameObject.SetActive(true);
+			Destroy(this);
+		}
+	}
+}
