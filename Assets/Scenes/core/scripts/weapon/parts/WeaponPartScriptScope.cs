97b5e5d272bc6fab4ab79b8caa8568c32a4446b3
James Keats <james.keats@mymail.champlain.edu>
2018-01-19 20:44:25 -0500

    Hook up the scope script to the new effects.

--- Assets/Scenes/core/scripts/weapon/parts/WeaponPartScriptScope.cs
+++ Assets/Scenes/core/scripts/weapon/parts/WeaponPartScriptScope.cs
@@ -1,5 +1,4 @@
-﻿using KeatsLib.Unity;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace FiringSquad.Gameplay.Weapons
 {
@@ -7,44 +6,28 @@ namespace FiringSquad.Gameplay.Weapons
 	public class WeaponPartScriptScope : WeaponPartScript
 	{
 		/// Inspector variables
-		[SerializeField] private Vector3 mAimDownSightsPosition;
-
-		/// Private variables
-		private Coroutine mMoveRoutine;
+		[SerializeField] private AimDownSightsEffect mAimDownSightsEffect;
 
 		/// <inheritdoc />
 		public override BaseWeaponScript.Attachment attachPoint { get { return BaseWeaponScript.Attachment.Scope; } }
 
 		/// <summary>
 		/// Activate the Aim Down Sights effect for this script.
-		/// TODO: Have a switch in here for multiple effect types.
 		/// </summary>
 		/// <param name="weapon">The weapon we are attached to.</param>
 		public void ActivateAimDownSightsEffect(IWeapon weapon)
 		{
-			Transform subView = weapon.transform.Find("View").GetChild(0);
-			if (mMoveRoutine != null)
-				StopCoroutine(mMoveRoutine);
-
-			mMoveRoutine = StartCoroutine(Coroutines.LerpPosition(subView, mAimDownSightsPosition, 0.2f, Space.Self, Coroutines.MATHF_SMOOTHSTEP));
+			mAimDownSightsEffect.ActivateEffect(weapon, this);
 		}
 
 		/// <summary>
 		/// Deativate the Aim Down Sights effect for this script.
-		/// TODO: Have a switch in here for multiple effect types.
 		/// </summary>
 		/// <param name="weapon">The weapon we are attached to.</param>
 		/// <param name="immediate">Whether or not to jump immediately to the "exit" state instead of lerping.</param>
 		public void DeactivateAimDownSightsEffect(IWeapon weapon, bool immediate = false)
 		{
-			Transform subView = weapon.transform.Find("View").GetChild(0);
-			if (mMoveRoutine != null)
-				StopCoroutine(mMoveRoutine);
-
-			if (!immediate)
-				mMoveRoutine = StartCoroutine(Coroutines.LerpPosition(subView, Vector3.zero, 0.2f, Space.Self, Coroutines.MATHF_SMOOTHSTEP));
-			else
-				subView.transform.localPosition = Vector3.zero;
+			mAimDownSightsEffect.DeactivateEffect(weapon, this, immediate);
 		}
 	}
 }
