c413c2dcc7eb311bbefcfb0ba945085fd0174967
Natalie Frost <natalie.frost@mymail.champlain.edu>
2018-01-25 09:59:30 -0500

    Created Animated Hit Indicator and p_animated-hit-indicator prefab. the Prefab will self destruct at the end of its animation, meaning clean-up is already handled. All thats needed to be done now is to SPAWN p_animated-hit-indicator  canvas every time the player shoots a valid target

---
+++ Assets/Scenes/core/scripts/ui/hud/hit_indicator/hit_indicator_selfdestruct.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hit_indicator_selfdestruct : MonoBehaviour
+{
+
+	void Delete ()
+	{
+		Destroy(this.gameObject);
+	}
+}
