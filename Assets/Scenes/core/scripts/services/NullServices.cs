0438b166f25350f653fab72a957046decbf3815a
James Keats <james.keats@mymail.champlain.edu>
2018-01-29 22:29:44 -0500

    Add the user name to the actual services.

--- Assets/Scenes/core/scripts/services/NullServices.cs
+++ Assets/Scenes/core/scripts/services/NullServices.cs
@@ -89,6 +89,9 @@ namespace FiringSquad.Core
 			public bool isAlive { get { return false; } }
 
 			/// <inheritdoc />
+			public string currentUserName { get; set; }
+
+			/// <inheritdoc />
 			public void RequestShutdown()
 			{
 				Logger.Info("NULL SERVICE: NullGamestateManager.RequestShutdown()", Logger.System.Services);
