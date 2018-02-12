106aaf1ef868e33fb354f5fe23e944a13f098b83
James Keats <james.keats@mymail.champlain.edu>
2018-02-12 14:57:21 -0500

    Add a script to kill the falling player.

---
+++ Assets/Scenes/draft6player/scripts/world/FallingPlayerKiller.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+namespace FiringSquad.Gameplay
+{
+	/// <summary>
+	/// Object that exists only on the server to kill any players that fall through the world.
+	/// </summary>
+	public class FallingPlayerKiller : NetworkBehaviour, IDamageSource
+	{
+		/// <inheritdoc />
+		public ICharacter source { get { return null; } }
+
+		/// <summary>
+		/// Unity Event handler when something hits our collider.
+		/// </summary>
+		[ServerCallback]
+		private void OnCollisionEnter(Collision other)
+		{
+			IDamageReceiver damageReceiver = other.collider.GetComponent<IDamageReceiver>();
+			if (damageReceiver != null)
+				damageReceiver.ApplyDamage(damageReceiver.currentHealth + 500.0f, other.transform.position, Vector3.up, this);
+		}
+	}
+}
