85da909b3e7f411ca9ebdecde9882604d8395b2e
James Keats <james.keats@mymail.champlain.edu>
2018-02-15 10:06:14 -0500

    Create a component to bind an emissive color to a boundproperty.

---
+++ Assets/Scenes/core/scripts/weapon/effects/BoundFloatEmissiveField.cs
@@ -0,0 +1,23 @@
+﻿using FiringSquad.Gameplay.UI;
+using UnityEngine;
+
+namespace FiringSquad.Gameplay.Weapons
+{
+	/// <summary>
+	/// Utiltiy component that binds the emissive color of the target mesh renderer to a provided float.
+	/// </summary>
+	public class BoundFloatEmissiveField : BoundUIElement<float>
+	{
+		/// Inspector variables
+		[SerializeField] private MeshRenderer mTargetMeshRenderer;
+		[SerializeField] private string mEmissiveProperty = "_EmissionColor";
+		[SerializeField] private Color mStartColor;
+		[SerializeField] private Color mEndColor;
+
+		/// <inheritdoc />
+		protected override void HandlePropertyChanged()
+		{
+			
+		}
+	}
+}
