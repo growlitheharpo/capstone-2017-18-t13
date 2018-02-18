f0b3294433667a8023afcd952fe6197caee8d861
James Keats <james.keats@mymail.champlain.edu>
2018-02-18 17:36:50 -0500

    feat(Core): Add two new console commands for connecting to matches.

--- Assets/Scenes/core/scripts/networking/NetworkGameManager.cs
+++ Assets/Scenes/core/scripts/networking/NetworkGameManager.cs
@@ -1,4 +1,8 @@
-﻿using UnityEngine.Networking;
+﻿using FiringSquad.Core;
+using FiringSquad.Debug;
+using UnityEngine;
+using UnityEngine.Networking;
+using Logger = FiringSquad.Debug.Logger;
 
 namespace FiringSquad.Networking
 {
@@ -9,6 +13,34 @@ namespace FiringSquad.Networking
 	{
 		/// Private variables
 		private int mPlayerCount;
+
+		/// <summary>
+		/// Unity's Start function.
+		/// </summary>
+		protected void Start()
+		{
+			ServiceLocator.Get<IGameConsole>()
+				.RegisterCommand("getip", CONSOLE_GetIpCommand)
+				.RegisterCommand("ipconfig", CONSOLE_GetIpCommand);
+		}
+
+		/// <summary>
+		/// Unity's OnDestroy signal.
+		/// </summary>
+		private void OnDestroy()
+		{
+			ServiceLocator.Get<IGameConsole>()
+				.UnregisterCommand(CONSOLE_GetIpCommand);
+		}
+
+		/// <summary>
+		/// Print the local player's ip address to the console.
+		/// </summary>
+		/// <param name="obj"></param>
+		private void CONSOLE_GetIpCommand(string[] obj)
+		{
+			Logger.Info("IP: " + Network.player.ipAddress);
+		}
 
 		/// <summary>
 		/// Unity function called when a new player is added.
