48aa38b3d410a6c20db36f1ed817d4c5f7f24c94
James Keats <james.keats@mymail.champlain.edu>
2018-02-23 21:30:36 -0500

    Start working on an editor script.

---
+++ Assets/Scenes/core/scripts/Editor/ui/UIColorUpdaterEditor.cs
@@ -0,0 +1,34 @@
+﻿using FiringSquad.Gameplay.UI;
+using UnityEngine;
+
+namespace UnityEditor
+{
+	/// <summary>
+	/// Custom editor for the color updater.
+	/// Adds a "Find All Children" button.
+	/// </summary>
+	[CustomEditor(typeof(UIColorUpdater))]
+	public class UIColorUpdaterEditor : Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			serializedObject.Update();
+			DrawDefaultInspector();
+
+			DrawFindObjectsButtons();
+
+			serializedObject.ApplyModifiedProperties();
+		}
+
+		private void DrawFindObjectsButtons()
+		{
+			CustomEditorGUIUtility.HorizontalLayout(() =>
+			{
+				if (GUILayout.Button(""))
+				{
+
+				}
+			});
+		}
+	}
+}
