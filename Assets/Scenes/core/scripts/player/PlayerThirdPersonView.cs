a54cb9e661e30ee6f72f8342c7f42db30d42d2db
James Keats <james.keats@mymail.champlain.edu>
2018-02-24 12:45:45 -0500

    Set up a component to change the sprite contextually.

---
+++ Assets/Scenes/core/scripts/player/PlayerThirdPersonView.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FiringSquad.Gameplay
+{
+	/// <summary>
+	/// Local script that handles the third person view of each character.
+	/// </summary>
+	public class PlayerThirdPersonView : MonoBehaviour
+	{
+		/// <summary>
+		/// The "CurrentSprite" indexes for each sprite.
+		/// </summary>
+		private enum FaceValues
+		{
+			Neutral = 0,
+			TeammateKilled = 1,
+			Dominated = 2,
+			GotLegendary = 3,
+			OtherPlayerGotLegendary = 4,
+			KillingSpree = 5,
+			GotKill = 6,
+			Wounded = 7,
+			CriticalHealth = 8,
+		}
+
+		/// Inspector variables
+		[SerializeField] private Renderer mTargetRenderer;
+
+		/// Private variables
+		private Material mTargetMaterial;
+
+		/// <summary>
+		/// Unity's Awake function
+		/// </summary>
+		private void Awake()
+		{
+			// Find the correct material on our target.
+			Shader targetShader = Shader.Find("Unlit/Unlit Spritesheet");
+			foreach (Material m in mTargetRenderer.materials)
+			{
+				if (m.shader != targetShader && !m.HasProperty("_CurrentSprite"))
+					continue;
+
+				mTargetMaterial = m;
+				break;
+			}
+		}
+	}
+}
--- Assets/Scenes/core/shaders/player_unlit_face_shader/UnlitSpritesheet.shader
+++ Assets/Scenes/core/shaders/player_unlit_face_shader/UnlitSpritesheet.shader
@@ -1,4 +1,4 @@
-﻿Shader "Unlit/UnlitSpritesheet"
+﻿Shader "Unlit/Unlit Spritesheet"
 {
 	Properties
 	{
