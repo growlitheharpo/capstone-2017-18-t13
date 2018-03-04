5e152b0e9474bdbafa40c31805351f1f8b39a8d8
James Keats <james.keats@mymail.champlain.edu>
2018-03-03 20:37:00 -0500

    feat(UI): Add a temporarily debug FPS counter in the top left corner.

---
+++ Assets/Scenes/core/scripts/utility/FPSDisplay.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace FiringSquad.Debug
+{
+	/// <summary>
+	/// Utility class that directly draws the current framerate to the top left of the screen.
+	/// </summary>
+	public class FPSDisplay : MonoBehaviour
+	{
+		private float mDeltaTime;
+		
+		/// <summary>
+		/// Unity's Update function
+		/// </summary>
+		private void Update()
+		{
+			mDeltaTime += (Time.unscaledDeltaTime - mDeltaTime) * 0.1f;
+		}
+
+		/// <summary>
+		/// Unity's OnGUI function
+		/// </summary>
+		private void OnGUI()
+		{
+			Rect rect = new Rect(0, 0, Screen.width, Screen.height * 2.0f / 100);
+
+			GUIStyle style = new GUIStyle
+			{
+				alignment = TextAnchor.UpperLeft,
+				fontSize = Screen.height * 2 / 100,
+				normal = { textColor = new Color(0.2f, 0.5f, 0.8f, 1.0f) }
+			};
+
+			float msec = mDeltaTime * 1000.0f;
+			float fps = 1.0f / mDeltaTime;
+
+			string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+			GUI.Label(rect, text, style);
+		}
+	}
+}
