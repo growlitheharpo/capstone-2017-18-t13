ccdbe7663869e301cb5f0fa738e8265857d09f5d
James Keats <james.keats@mymail.champlain.edu>
2018-03-18 13:02:04 -0400

    Get a pretty significant chunk of the front end of the intro set up.

---
+++ Assets/Scenes/core/scripts/ui/screen/PlayerTeamNotificationPanel.cs
@@ -0,0 +1,46 @@
+﻿using FiringSquad.Data;
+using UnityEngine;
+using UIText = UnityEngine.UI.Text;
+
+namespace FiringSquad.Gameplay.UI
+{
+	public class PlayerTeamNotificationPanel : MonoBehaviour
+	{
+		/// Inspector variables
+		[SerializeField] private UIText mLabel1;
+		[SerializeField] private UIText mTeamName;
+		[SerializeField] private UIText mLabel2;
+
+		/// <summary>
+		/// Unity's Start function
+		/// </summary>
+		private void Start()
+		{
+			EventManager.LocalGUI.OnLocalPlayerAssignedTeam += OnLocalPlayerAssignedTeam;
+			gameObject.SetActive(false);
+		}
+
+		/// <summary>
+		/// Unity's OnDestroy function
+		/// </summary>
+		private void OnDestroy()
+		{
+			EventManager.LocalGUI.OnLocalPlayerAssignedTeam -= OnLocalPlayerAssignedTeam;
+		}
+
+		/// <summary>
+		/// EVENT HANDLER: LocalGUI.OnLocalPlayerAssignedTeam
+		/// </summary>
+		private void OnLocalPlayerAssignedTeam(CltPlayer localPlayer)
+		{
+			GameData.PlayerTeam team = localPlayer.playerTeam;
+			mTeamName.text = team.ToString().ToUpper();
+
+			if (team == GameData.PlayerTeam.Deathmatch)
+			{
+				mLabel1.text = "THIS IS A";
+				mLabel2.text = "GAME";
+			}
+		}
+	}
+}
