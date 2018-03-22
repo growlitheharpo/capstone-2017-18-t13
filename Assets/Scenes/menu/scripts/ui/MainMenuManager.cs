b15c3fcded934b2b48041fb72b0a9c6f1df7ac76
Charles Carucci <charles.carucci@mymail.champlain.edu>
2018-03-22 17:34:32 -0400

    credits button actually works now

--- Assets/Scenes/menu/scripts/ui/MainMenuManager.cs
+++ Assets/Scenes/menu/scripts/ui/MainMenuManager.cs
@@ -17,18 +17,27 @@ namespace FiringSquad.Gameplay.UI
 		[SerializeField] private UIButton mQuitButton;
 		[SerializeField] private UIButton mGunGlossaryButton;
 		[SerializeField] private UIButton mHowToPlayButton;
+		[SerializeField] private UIButton mCreditsButton;
+		[SerializeField] private UIButton mMainMenuButton;
 		[SerializeField] private int mKioskTimerSeconds = 10;
+		[SerializeField] private GameObject mMainMenuAssets;
+		[SerializeField] private GameObject mCreditAssets;
+
 		private int mKioskTimerTicks;
 
 		/// <summary>
 		/// Unity's Start function
 		/// </summary>
 		private void Start()
-		{ 
+		{
+			ReturnToMenu();
+
 			mFourPlayerButton.onClick.AddListener(LaunchNewLevel);
 			mQuitButton.onClick.AddListener(ClickQuit);
 			mGunGlossaryButton.onClick.AddListener(LaunchGlossary);
 			mHowToPlayButton.onClick.AddListener(LaunchHowToPlay);
+			mCreditsButton.onClick.AddListener(LaunchCredits);
+			mMainMenuButton.onClick.AddListener(ReturnToMenu);
 
 			mKioskTimerTicks = mKioskTimerSeconds * 100;
 		}
@@ -73,6 +82,36 @@ namespace FiringSquad.Gameplay.UI
 
 			ServiceLocator.Get<IGamestateManager>()
 				.RequestSceneChange(GamestateManager.GUN_GLOSSARY);
+		}
+
+		/// <summary>
+		/// Hide the menu buttons & display the credits
+		/// </summary>
+		private void LaunchCredits()
+		{
+
+			if (mMainMenuAssets != null)
+			{
+				mMainMenuAssets.gameObject.SetActive(false);
+				mCreditAssets.gameObject.SetActive(true);
+				mMainMenuButton.gameObject.SetActive(true);
+			}
+		}
+
+		/// <summary>
+		/// Hide all other menu assets and return to the main menu
+		/// </summary>
+		private void ReturnToMenu()
+		{
+			UnityEngine.Debug.Log("Open Main Menu");
+			if (mMainMenuButton != null)
+			{
+				mCreditAssets.gameObject.SetActive(false);
+				mMainMenuButton.gameObject.SetActive(false);
+				//Gun Glossary Assets should deactivate
+				//How To Play Assets should deactivate
+				mMainMenuAssets.gameObject.SetActive(true);
+			}
 		}
 
 		/// <summary>
