046c288b43ba53c8d59d539554f57df44ed5dc8a
James Keats <james.keats@mymail.champlain.edu>
2018-03-23 20:32:30 -0400

    Setup the manager to listen for the events.

--- Assets/Scenes/core/scripts/world/CrowdAudio.cs
+++ Assets/Scenes/core/scripts/world/CrowdAudio.cs
@@ -1,6 +1,7 @@
 ﻿using FiringSquad.Core;
 using FiringSquad.Core.Audio;
 using FiringSquad.Data;
+using KeatsLib.Unity;
 using UnityEngine;
 
 namespace FiringSquad.Gameplay
@@ -14,18 +15,29 @@ namespace FiringSquad.Gameplay
 		[SerializeField] private CrowdAudioEventValues mValues;
 
 		private IAudioReference mCrowdSound;
-		private float mCurrentCrowdLevel;
+		private int mCurrentCrowdLevel;
+		private float mTimer;
 
 		/// <summary>
 		/// Unity's Start function
 		/// </summary>
 		private void Start()
 		{
+			StartCoroutine(Coroutines.InvokeAfterFrames(5, InitializeSound));
+
+			EventManager.LocalGeneric.OnPlayerCapturedStage += OnPlayerCapturedStage;
+			EventManager.LocalGeneric.OnPlayerDied += OnPlayerDied;
+			EventManager.LocalGeneric.OnPlayerEquippedLegendaryPart += OnPlayerEquippedLegendaryPart;
+		}
+
+		private void InitializeSound()
+		{
 			mCurrentCrowdLevel = mValues.minHypeValue;
 			mCrowdSound = ServiceLocator.Get<IAudioManager>()
 				.CreateSound(AudioEvent.AmbientCrowd, null);
 
 			mCrowdSound.crowdHypeLevel = mCurrentCrowdLevel;
+			mTimer = 0.0f;
 		}
 
 		/// <summary>
@@ -33,7 +45,67 @@ namespace FiringSquad.Gameplay
 		/// </summary>
 		private void OnDestroy()
 		{
-			
+			EventManager.LocalGeneric.OnPlayerCapturedStage -= OnPlayerCapturedStage;
+			EventManager.LocalGeneric.OnPlayerDied -= OnPlayerDied;
+			EventManager.LocalGeneric.OnPlayerEquippedLegendaryPart -= OnPlayerEquippedLegendaryPart;
+		}
+
+		/// <summary>
+		/// Unity's Update function
+		/// </summary>
+		private void Update()
+		{
+			mTimer -= Time.deltaTime;
+			if (mTimer > 0.0f)
+				return;
+
+			// If the timer is up, try to decrement the hype level. If it changed, push it to FMOD
+			// NOTE: The reason for the complexity is that sending data to FMOD is more expensive than the check.
+			int newValue = Mathf.Clamp(mCurrentCrowdLevel - 1, mValues.minHypeValue, mValues.maxHypeValue);
+			if (newValue != mCurrentCrowdLevel)
+			{
+				mCurrentCrowdLevel = newValue;
+				mCrowdSound.crowdHypeLevel = mCurrentCrowdLevel;
+			}
+
+			mTimer = mValues.subsequentDecreaseTimerLength;
+		}
+
+		/// <summary>
+		/// EVENT HANDLER: LocalGeneric.OnPlayerCapturedStage
+		/// </summary>
+		private void OnPlayerCapturedStage()
+		{
+			IncreaseHypeLevel(mValues.stageCaptureGain);
+		}
+		
+		/// <summary>
+		/// EVENT HANDLER: LocalGeneric.OnPlayerDied
+		/// </summary>
+		private void OnPlayerDied()
+		{
+			IncreaseHypeLevel(mValues.standardDeathGain);
+		}
+		
+		/// <summary>
+		/// EVENT HANDLER: LocalGeneric.OnPlayerEquippedLegendaryPart
+		/// </summary>
+		private void OnPlayerEquippedLegendaryPart()
+		{
+			IncreaseHypeLevel(mValues.legendaryPartGain);
+		}
+
+		/// <summary>
+		/// Immediately reset any decreasing timers and increase the hype level.
+		/// </summary>
+		/// <param name="amount"></param>
+		private void IncreaseHypeLevel(int amount)
+		{
+			mCurrentCrowdLevel = Mathf.Clamp(mCurrentCrowdLevel + amount, mValues.minHypeValue, mValues.maxHypeValue);
+			UnityEngine.Debug.Log(mCurrentCrowdLevel);
+			mCrowdSound.crowdHypeLevel = mCurrentCrowdLevel;
+
+			mTimer = mValues.initialDecreaseTimerLength;
 		}
 	}
 }
