6ffa2b98dbc462daed218ecb521ef8f46a9ddff9
Charles Carucci <charles.carucci@mymail.champlain.edu>
2018-03-31 09:55:17 -0400

    Set up menu button sounds and fixed the brackets on thre sub-play menu

---
+++ Assets/Scenes/menu/scripts/ui/MenuButtonSFX.cs
@@ -0,0 +1,47 @@
+﻿using FiringSquad.Core;
+using FiringSquad.Core.Audio;
+using FiringSquad.Core.State;
+using KeatsLib.Unity;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuButtonSFX : MonoBehaviour {
+
+	/// Inspector variables
+	[SerializeField] private bool playButton;
+	
+	/// Private variables
+	private IAudioReference mButtonHoverSound;
+	private IAudioReference mButtonPressSound;
+
+	public void PlayHoverSound()
+	{
+		IAudioManager audioService = ServiceLocator.Get<IAudioManager>();
+		mButtonHoverSound = audioService.CheckReferenceAlive(ref mButtonHoverSound);
+
+		if (mButtonHoverSound == null)
+		{
+			mButtonHoverSound = ServiceLocator.Get<IAudioManager>().CreateSound(AudioEvent.MenuButtonHover, gameObject.transform, false);
+			mButtonHoverSound.Start();
+		}
+	}
+
+	public void PlayPressSound()
+	{
+		IAudioManager audioService = ServiceLocator.Get<IAudioManager>();
+		mButtonPressSound = audioService.CheckReferenceAlive(ref mButtonPressSound);
+
+		if (mButtonPressSound == null)
+		{
+			mButtonPressSound = ServiceLocator.Get<IAudioManager>().CreateSound(AudioEvent.MenuButtonPress, gameObject.transform, false);
+			if (playButton)
+			{
+				mButtonPressSound.SetParameter("IsPlayButton", 1f);
+			} else
+			{
+				mButtonPressSound.SetParameter("IsPlayButton", 0f);
+			}
+			mButtonPressSound.Start();
+		}
+	}
+}
