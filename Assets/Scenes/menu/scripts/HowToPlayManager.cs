e444bd61fbebc4f7f40d99ebc36b005921218de6
James Keats <james.keats@mymail.champlain.edu>
2018-03-30 23:29:22 -0400

    Remove the how to play scene.

---
+++ Assets/Scenes/menu/scripts/HowToPlayManager.cs
@@ -0,0 +1,25 @@
+﻿using FiringSquad.Core;
+using FiringSquad.Core.State;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UIButton = UnityEngine.UI.Button;
+using UnityEngine.UI;
+
+
+namespace FiringSquad.Gameplay.UI
+{
+	/// <summary>
+	/// How to play menu slide manager
+	/// </summary>
+	public class HowToPlayManager : MonoBehaviour
+	{
+		[SerializeField] private GameObject mMainElementHolder;
+
+		// Use this for initialization
+		private void Start()
+		{
+
+		}
+	}
+}
+
